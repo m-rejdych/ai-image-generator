62c49843295b8ec9841ff4a98307c137ba5d0335
m-rejdych <rejdych.m.r@gmail.com>
2023-11-22 22:13:41 +0100

    Added db setup.

--- server/src/types/global.d.ts
+++ server/src/types/global.d.ts
@@ -1,6 +1,13 @@
+import type { PrismaClient } from '@prisma/client';
+
 declare namespace NodeJS {
   interface ProcessEnv {
     PORT: string;
     HOST: string;
+    NODE_ENV: 'production' | 'development';
   }
+}
+
+declare global {
+  var prisma: PrismaClient;
 }
